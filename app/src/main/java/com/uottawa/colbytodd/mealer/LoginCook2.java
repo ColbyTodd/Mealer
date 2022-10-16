95e344dae4563b96194b5de23a623caab6d05198
unknown <ghoot072@uottawa.ca>
2022-10-16 18:51:57 -0400

    Created most of the pages that will be used

---
+++ app/src/main/java/com/uottawa/colbytodd/mealer/LoginCook2.java
@@ -0,0 +1,15 @@
+
+package com.uottawa.colbytodd.mealer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginCook2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_cook2);
+    }
+}
