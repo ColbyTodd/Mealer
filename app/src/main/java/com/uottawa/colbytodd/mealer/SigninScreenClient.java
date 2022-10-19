59de8ee5d0ff7deb43e228aa40525b1deb6d2c00
Colby Todd <ctodd083@uottawa.ca>
2022-10-19 16:38:34 -0400

    Added client registration

--- app/src/main/java/com/uottawa/colbytodd/mealer/SigninScreenClient.java
+++ app/src/main/java/com/uottawa/colbytodd/mealer/SigninScreenClient.java
@@ -1,19 +1,79 @@
 package com.uottawa.colbytodd.mealer;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class SigninScreenClient extends AppCompatActivity {
+    private static final String TAG = "EmailPassword";
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin_screen);
+
+        // ...
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            reload();
+        }
+    }
+
+    private void createAccount(String email, String password) {
+        // [START create_user_with_email]
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(SigninScreenClient.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+        // [END create_user_with_email]
+    }
+
     public void openClientWelcome(View v){
-        startActivity(new Intent(this, ClientWelcome.class));
+        EditText email = (EditText) findViewById(R.id.Email);
+        EditText password = (EditText) findViewById(R.id.Password);
+        createAccount(email.getText().toString(), password.getText().toString());
+
+
+    private void reload(){}
+
+    private void updateUI(FirebaseUser user) {
+            startActivity(new Intent(this, ClientWelcome.class));}
     }
 }
