f4d4dbba11e99d72178e5f6bde4104fff9841756
Colby Todd <ctodd083@uottawa.ca>
2022-10-31 18:02:12 -0400

    Added popup menu to AdminWelcome

--- app/src/main/java/com/uottawa/colbytodd/mealer/AdminWelcome.java
+++ app/src/main/java/com/uottawa/colbytodd/mealer/AdminWelcome.java
@@ -1,18 +1,40 @@
 package com.uottawa.colbytodd.mealer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.PopupMenu;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
-public class AdminWelcome extends AppCompatActivity {
+public class AdminWelcome extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_welcome);
     }
+
+    //Shows a popup menu when a button is clicked
+    public void showMenu(View v) {
+        PopupMenu popup = new PopupMenu(this, v);
+
+        // This activity implements OnMenuItemClickListener
+        popup.setOnMenuItemClickListener(this);
+        popup.inflate(R.menu.admin);
+        popup.show();
+    }
+
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.complaints:
+                return true;
+            default:
+                return false;
+        }
+    }
+
     public void logOff(View v){
         startActivity(new Intent(this, MainActivity.class));
     }
