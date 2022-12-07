c5e20b27ad61572c1e54bb3a6ab0755324102a81
Colby Todd <ctodd083@uottawa.ca>
2022-12-07 14:41:59 -0500

    Created new Activity ComplaintRate

---
+++ app/src/main/java/com/uottawa/colbytodd/mealer/ComplaintRate.java
@@ -0,0 +1,14 @@
+package com.uottawa.colbytodd.mealer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ComplaintRate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complaint_rate);
+    }
+}
--- app/src/main/java/com/uottawa/colbytodd/mealer/clientPurchaseHistory.java
+++ app/src/main/java/com/uottawa/colbytodd/mealer/clientPurchaseHistory.java
@@ -78,6 +78,7 @@ public class clientPurchaseHistory extends AppCompatActivity {
                             purchases.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                                 @Override
                                 public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+
                                     Intent i = new Intent(clientPurchaseHistory.this, FileComplaint.class);
                                     i.putExtra("Email", email);
                                     i.putExtra("Cook", documents.get(position).getCook());
