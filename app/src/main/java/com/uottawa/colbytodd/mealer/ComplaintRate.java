d01fc913e4eaddc934754a583063003b50541ae1
Colby Todd <ctodd083@uottawa.ca>
2022-12-07 15:36:52 -0500

    Added complaints popup

--- app/src/main/java/com/uottawa/colbytodd/mealer/ComplaintRate.java
+++ app/src/main/java/com/uottawa/colbytodd/mealer/ComplaintRate.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -32,6 +33,20 @@ public class ComplaintRate extends AppCompatActivity {
 
         TextView title = (TextView) findViewById(R.id.ComplaintRateTitleText);
         title.setText("Complain or rate about " + cook);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if(id == android.R.id.home){
+            //Ends the activity
+            this.finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void onRateClick(View view){
@@ -69,6 +84,9 @@ public class ComplaintRate extends AppCompatActivity {
 
         // 1. Instantiate an <code><a href="/reference/android/app/AlertDialog.Builder.html">AlertDialog.Builder</a></code> with its constructor
         AlertDialog.Builder builder = new AlertDialog.Builder(ComplaintRate.this);
+
+        //Set the view
+        builder.setView(R.layout.complaint_dialog);
 
         // Add the buttons
         builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
