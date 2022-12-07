0c11177c9d12ec88344eb6a3a752152fa9d187c3
Colby Todd <ctodd083@uottawa.ca>
2022-12-07 17:05:57 -0500

    Added rating to cook profile

--- app/src/main/java/com/uottawa/colbytodd/mealer/cookProfile.java
+++ app/src/main/java/com/uottawa/colbytodd/mealer/cookProfile.java
@@ -12,9 +12,12 @@ import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class cookProfile extends AppCompatActivity {
     Bundle extras;
@@ -47,17 +50,43 @@ public class cookProfile extends AppCompatActivity {
                         TextView fullName = findViewById(R.id.textView14);
                         TextView address = findViewById(R.id.textView15);
                         TextView emailText = findViewById(R.id.emailTexts);
-                        TextView rating = (TextView) findViewById(R.id.textView19);
                         emailText.setText(email);
                         fullName.setText(document.getData().get("first").toString()+" "+document.getData().get("last").toString());
                         address.setText(document.getData().get("address").toString());
-                        rating.setText("");
                         //rating.setText(document.getData().get("rating"));  FIX THIS ONCE RATING IS IMPLEMENTED
                         Log.d("PROFILE", "DocumentSnapshot data: " + document.getData());
                     } else {
                         Log.d("PROFILE", "No such document");
                     }
                 } else {
+                    Log.d("PROFILE", "get failed with ", task.getException());
+                }
+            }
+        });
+
+        CollectionReference docRef2 = db.collection("cooks").document(email).collection("ratings");
+        docRef2.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    long rating = 0, count = 0;
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        if (document.exists()) {
+
+                            rating = rating + Long.parseLong(document.get("rating").toString());
+                            count++;
+                            //rating.setText(document.getData().get("rating"));  FIX THIS ONCE RATING IS IMPLEMENTED
+                            Log.d("PROFILE", "DocumentSnapshot data: " + document.getData());
+                        } else {
+                            Log.d("PROFILE", "No such document");
+                        }
+                    }
+
+                    rating = rating * 100/count;
+                    TextView rate = (TextView) findViewById(R.id.textView19);
+                    rate.setText( (rating/100.0) + "");
+
+                }else {
                     Log.d("PROFILE", "get failed with ", task.getException());
                 }
             }
